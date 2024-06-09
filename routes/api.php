9904d709a4530e56a29c77cda55cf693cfa93ba4
David <davidolanrewaju538@gmail.com>
2024-06-09 22:28:14 +0100

    Api route setup

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->group(function (Request $request) {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 })->middleware('auth:api');
